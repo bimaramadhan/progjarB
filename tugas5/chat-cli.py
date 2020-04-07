64596220cdb23da2266be4dd06859eb4a909ebcc
Bima S. Ramadhan <37492909+bimaramadhan@users.noreply.github.com>
2020-04-07 21:49:51 +0700

    ok

--- tugas5/chat-cli.py
+++ tugas5/chat-cli.py
@@ -5,13 +5,13 @@ import json
 TARGET_IP = "127.0.0.1"
 TARGET_PORT = 8889
 
-
 class ChatClient:
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_address = (TARGET_IP,TARGET_PORT)
         self.sock.connect(self.server_address)
         self.tokenid=""
+
     def proses(self,cmdline):
         j=cmdline.split(" ")
         try:
@@ -28,10 +28,15 @@ class ChatClient:
                 return self.sendmessage(usernameto,message)
             elif (command=='inbox'):
                 return self.inbox()
+            elif (command=='listuser'):
+                return self.listuser()
+            elif(command=='logout'):
+                return self.logout()
             else:
                 return "*Maaf, command tidak benar"
         except IndexError:
                 return "-Maaf, command tidak benar"
+
     def sendstring(self,string):
         try:
             self.sock.sendall(string.encode())
@@ -40,13 +45,14 @@ class ChatClient:
                 data = self.sock.recv(64)
                 print("diterima dari server",data)
                 if (data):
-                    receivemsg = "{}{}" . format(receivemsg,data.decode())  #data harus didecode agar dapat di operasikan dalam bentuk string
+                    receivemsg = "{}{}" . format(receivemsg,data.decode())
                     if receivemsg[-4:]=='\r\n\r\n':
                         print("end of string")
                         return json.loads(receivemsg)
         except:
             self.sock.close()
             return { 'status' : 'ERROR', 'message' : 'Gagal'}
+
     def login(self,username,password):
         string="auth {} {} \r\n" . format(username,password)
         result = self.sendstring(string)
@@ -55,6 +61,7 @@ class ChatClient:
             return "username {} logged in, token {} " .format(username,self.tokenid)
         else:
             return "Error, {}" . format(result['message'])
+
     def sendmessage(self,usernameto="xxx",message="xxx"):
         if (self.tokenid==""):
             return "Error, not authorized"
@@ -65,6 +72,7 @@ class ChatClient:
             return "message sent to {}" . format(usernameto)
         else:
             return "Error, {}" . format(result['message'])
+
     def inbox(self):
         if (self.tokenid==""):
             return "Error, not authorized"
@@ -75,11 +83,31 @@ class ChatClient:
         else:
             return "Error, {}" . format(result['message'])
 
+    def listuser(self):
+        if (self.tokenid == ""):
+            return "Error, not authorized"
+        string = "listuser {} \r\n".format(self.tokenid)
+        result = self.sendstring(string)
+        if result['status'] == 'OK':
+            print("User sedang aktif ")
+            return "{}".format(json.dumps(result['listuseraktif']))
+        else:
+            return "Error, {}".format(result['message'])
 
+    def logout(self):
+        if (self.tokenid == ""):
+            return "Error, not authorized"
+        string="logout {} \r\n" . format(self.tokenid)
+        print(string)
+        result = self.sendstring(string)
+        if result['status']=='OK':
+            self.tokenid=""
+            return "{}" . format(json.dumps(result['messages']))
+        else:
+            return "Error, {}" . format(result['message'])
 
 if __name__=="__main__":
     cc = ChatClient()
     while True:
         cmdline = input("Command {}:" . format(cc.tokenid))
         print(cc.proses(cmdline))
-
